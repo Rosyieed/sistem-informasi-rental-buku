48b7ee1996225dab1391e00d9b7d21b1ac8ea72c
Rosyieed <rosyid123.ren@gmail.com"git>
2023-06-20 15:34:00 +0700

    first commit

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        $books = Book::paginate(5);
+        return view('book', ['books' => $books]);
+    }
+
+    public function add()
+    {
+        $categories = Category::all();
+        return view('book-add', ['categories' => $categories]);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'book_code' => 'required|unique:books|max:255'
+        ]);
+
+        $books = Book::create($request->all());
+        $books->categories()->sync($request->categories);
+        return redirect('books')->withSuccess('Buku Berhasil Ditambahkan');
+    }
+
+    public function edit($slug)
+    {
+        $book = Book::where('slug', $slug)->first();
+        $categories = Category::all();
+        return view('book-edit', ['categories' => $categories, 'book' => $book]);
+    }
+
+    public function update(Request $request, $slug)
+    {
+
+        $book = Book::where('slug', $slug)->first();
+        $book->update($request->all());
+
+        if ($request->categories) {
+            $book->categories()->sync($request->categories);
+        }
+
+        return redirect('books')->withSuccess('Buku Berhasil Diupdate');
+    }
+
+    public function delete($slug)
+    {
+        $book = Book::where('slug', $slug)->first();
+        return view('book-delete', ['book' => $book]);
+    }
+
+    public function destroy($slug)
+    {
+        $book = Book::where('slug', $slug)->first();
+        $book->forceDelete();
+        return redirect('/books')->withSuccess('Buku Berhasil Dihapus');
+    }
+}
